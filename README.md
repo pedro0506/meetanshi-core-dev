ab228b02a8a233b188ddc6953401b7efaadfc5e8
pedro0506 <p.souzaunip@gmail.com>
2022-09-20 11:22:12 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# meetanshi-core-dev
---
+++ registration.php
@@ -0,0 +1,6 @@
+<?php 
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Psdev_Extensions',
+    __DIR__
+);
